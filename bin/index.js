9afc6df17f042eb041b143293aff8d0eaa91315b
山丘 <gengze@innotechx.com>
2020-01-18 19:06:49 +0800

    feat: 变量重命名

--- bin/index.js
+++ bin/index.js
@@ -58,65 +58,65 @@ const b1s1Json = readFirstSheet(book1)
 if (b1s1Json.length <= 0) {
   throw new Error(`${book1}没有可处理的数据`)
 }
-const b1s1headers = Object.keys(b1s1Json[0])
+const b1s1Headers = Object.keys(b1s1Json[0])
 const {
   [SHEET_ONE.NAME_IDX]: b1s1NameHeader,
   [SHEET_ONE.VALUE_IDX]: b1s1ValueHeader,
-} = b1s1headers;
-const normalizedData = normalizeRow(b1s1Json, b1s1NameHeader)
+} = b1s1Headers;
+const b1s1NormalizedJson = normalizeRow(b1s1Json, b1s1NameHeader)
 const b2s1Json = readFirstSheet(book2)
 if (b2s1Json.length <= 0) {
   throw new Error(`${book2}没有可处理的数据`)
 }
-const headers = Object.keys(b2s1Json[0])
+const b2s1Headers = Object.keys(b2s1Json[0])
 const {
-  [SHEET_TWO.FILENAME_IDX]: filenameHeader,
-  [SHEET_TWO.NAME_IDX]: nameHeader,
-  [SHEET_TWO.VALUE_IDX]: valueHeader,
-} = headers
-const filename = b2s1Json[0][filenameHeader]
+  [SHEET_TWO.FILENAME_IDX]: b2s1FilenameHeader,
+  [SHEET_TWO.NAME_IDX]: b2s1NameHeader,
+  [SHEET_TWO.VALUE_IDX]: b2s1ValueHeader,
+} = b2s1Headers
+const filename = b2s1Json[0][b2s1FilenameHeader]
 const resultArr = []
 b2s1Json.forEach(row => {
-  const name = row[nameHeader]
+  const name = row[b2s1NameHeader]
   if (!name) {
     return;
   }
   let result;
   // 两个表格都存在的数据，表1减去表2的数值
-  if (normalizedData[name]) {
-    delete normalizedData[name]["__EMPTY"]
+  if (b1s1NormalizedJson[name]) {
+    delete b1s1NormalizedJson[name]["__EMPTY"]
     result = Object.assign({
-      [filenameHeader]: row[filenameHeader],
-    }, normalizedData[name], {
-      [valueHeader]: row[valueHeader],
-      "结果": Number((getValueByCol(normalizedData[name], SHEET_ONE.VALUE_IDX) - row[valueHeader]).toFixed(2)),
+      [b2s1FilenameHeader]: row[b2s1FilenameHeader],
+    }, b1s1NormalizedJson[name], {
+      [b2s1ValueHeader]: row[b2s1ValueHeader],
+      "结果": Number((getValueByCol(b1s1NormalizedJson[name], SHEET_ONE.VALUE_IDX) - row[b2s1ValueHeader]).toFixed(2)),
     })
-    delete normalizedData[name]
+    delete b1s1NormalizedJson[name]
   } else {
     // 只有表2存在的数据
     result = Object.assign({
-      [filenameHeader]: row[filenameHeader],
-      [b1s1NameHeader]: row[nameHeader],
+      [b2s1FilenameHeader]: row[b2s1FilenameHeader],
+      [b1s1NameHeader]: row[b2s1NameHeader],
       [b1s1ValueHeader]: 0,
     }, row, {
-      "结果": Number((0 - row[valueHeader]).toFixed(2)),
+      "结果": Number((0 - row[b2s1ValueHeader]).toFixed(2)),
     })
     // 固定采用表1的name表头
-    delete result[nameHeader]
+    delete result[b2s1NameHeader]
   }
   resultArr.push(result)
 })
 // 只有表1存在数据
-Object.keys(normalizedData)
+Object.keys(b1s1NormalizedJson)
   .forEach((name) => {
-    const row = normalizedData[name]
+    const row = b1s1NormalizedJson[name]
     if (!row || !row[name]) {
       return;
     }
     const newRow = Object.assign({
-      [filenameHeader]: filename,
+      [b2s1FilenameHeader]: filename,
     }, row, {
-      [valueHeader]: 0,
+      [b2s1ValueHeader]: 0,
       "结果": Number((row[b1s1ValueHeader] - 0).toFixed(2)),
     });
     resultArr.push(newRow)
@@ -125,16 +125,16 @@ Object.keys(normalizedData)
 const dataLength = resultArr.length
 const codeA = 'A'.charCodeAt(0)
 const b1s1ValueColChar = String.fromCharCode(codeA + getIdxByHeader(resultArr, b1s1ValueHeader))
-const b2s1VauleColChar = String.fromCharCode(codeA + getIdxByHeader(resultArr, valueHeader))
+const b2s1VauleColChar = String.fromCharCode(codeA + getIdxByHeader(resultArr, b2s1ValueHeader))
 const resultColChar = String.fromCharCode(codeA + getIdxByHeader(resultArr, "结果"))
 resultArr.push({
-  [filenameHeader]: "原始数据求合相减",
+  [b2s1FilenameHeader]: "原始数据求合相减",
   [b1s1ValueHeader]: {f: `SUM(${b1s1ValueColChar}2:${b1s1ValueColChar}${dataLength + 1})`},
-  [valueHeader]: {f: `SUM(${b2s1VauleColChar}2:${b2s1VauleColChar}${dataLength + 1})`},
+  [b2s1ValueHeader]: {f: `SUM(${b2s1VauleColChar}2:${b2s1VauleColChar}${dataLength + 1})`},
   "结果": {f: `${b1s1ValueColChar}${dataLength + 2}-${b2s1VauleColChar}${dataLength + 2}`}
 })
 resultArr.push({
-  [filenameHeader]: "计算结果求合",
+  [b2s1FilenameHeader]: "计算结果求合",
   "结果": {f: `SUM(${resultColChar}2:${resultColChar}${dataLength + 1})`}
 })
 
